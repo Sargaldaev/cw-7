1f5c3f7d74907bc92f8f8a7d48f6d12b980c2d16
elaman sargaldaev <melisbekovelaman311@gmail.com>
2023-06-03 16:10:56 +0600

    написал логику на меню

---
+++ src/component/Menu/Menu.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Menu.css'
+
+interface Iprops {
+  img:string;
+  name:string;
+  count:number | string;
+  onImgClick:React.MouseEventHandler;
+  onBtnClick:React.MouseEventHandler;
+}
+const Menu:React.FC<Iprops> = props => {
+  return (
+
+    <div className="menu" onClick={props.onImgClick}>
+        <img src={props.img} alt={props.name} className="img"/>
+        <p>{props.name}</p>
+        <p>{props.count}</p>
+      </div>
+
+  );
+};
+export default Menu;
